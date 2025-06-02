e5e848ae5116766dc937d8f35c843db94fe03e98
Marta Munoz Merino <martamunozmerino@gmail.com>
2025-06-02 17:47:49 +0200

    first commit

--- src/main/java/Spielzug/Papier.java
+++ src/main/java/Spielzug/Papier.java
@@ -4,4 +4,28 @@ public class Papier implements Spielzug {
 
     private String name;
 
-    public Papi
+    public Papier() {
+        name = "Papier";
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public int vergleiche(Spielzug anderer) {
+      if(anderer instanceof Stein){
+          return 1; //gewinnt
+      }else if(anderer instanceof Schere){
+          return -1; //verliert
+      }else{
+          return 0; //Papier
+      }
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+}
--- src/main/java/Spielzug/Stein.java
+++ src/main/java/Spielzug/Stein.java
@@ -4,4 +4,30 @@ public class Stein implements Spielzug {
 
     private String name;
 
-    public Ste
+    public Stein() {
+
+        name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public int vergleiche(Spielzug anderer) {
+        if(anderer instanceof Schere){
+            return 1; //gewinnt
+        }else if(anderer instanceof Papier){
+            return -1; //verliert
+        }else{
+            return 0; //Stein
+        }
+    }
+
+}
